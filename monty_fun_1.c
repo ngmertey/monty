6253e558bb60f1896679374c34aa5bcae134c494
Joshua <ngmerteyjoshua360@gmail.com>
2024-01-20 12:43:28 -0800

    Implement monty rotl and monty rotr

--- monty_fun_1.c
+++ monty_fun_1.c
@@ -9,7 +9,7 @@ void monty_swap(stack_t **stack, unsigned int line_number);
 /**
  * monty_push - Pushes a value to a stack_t linked list.
  * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
+ * @line_number: The current working line number of a Monty bytecode file.
  */
 void monty_push(stack_t **stack, unsigned int line_number)
 {
